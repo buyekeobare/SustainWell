a066f944353676d751af04efaed906262900bf5e
buyekeobare <buyekeobare@gmail.com>
2025-02-14 12:29:58 +0300

    Created views

--- SustainWell-backend/resources/views.py
+++ SustainWell-backend/resources/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from rest_framework import viewsets
+from .models import Resource, MaintenanceLog
+from .serializers import ResourceSerializer, MaintenanceLogSerializer
+
+class ResourceViewSet(viewsets.ModelViewSet):
+    queryset = Resource.objects.all()
+    serializer_class = ResourceSerializer
+
+class MaintenanceLogViewSet(viewsets.ModelViewSet):
+    queryset = MaintenanceLog.objects.all()
+    serializer_class = MaintenanceLogSerializer
+
