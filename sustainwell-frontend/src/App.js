35a63954446dad76e5f1aa44ba32d4adb17318cc
buyekeobare <buyekeobare@gmail.com>
2025-02-21 22:14:21 +0300

    App.js updated

--- sustainwell-frontend/src/App.js
+++ sustainwell-frontend/src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
+import ResourceMonitor from './components/ResourceMonitor';
+import Maintenance from './components/Maintenance';
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={LandingPage} />
+        <Route path="/resources" component={ResourceMonitor} />
+        <Route path="/maintenance" component={Maintenance} />
+        <Route path="/dashboard" component={Dashboard} />
       </Switch>
     </Router>
   );
 }
 
 export default App;
+
